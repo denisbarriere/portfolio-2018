ed5735d83830595823b1e35a24be5b4c2f3893d8
denisbarriere <XBU[DZ2xHk&V84bacod3>
2018-11-26 12:44:57 +0100

    Add color and transition utilities

--- src/Utilities/colors.js
+++ src/Utilities/colors.js
@@ -1,4 +1,6 @@
-export const red = "fd5f65";
+export const black = "#000";
+export const grey = "#aaa";
+export const red = "#fd5f65";
 export const lightRed = "#fa715e";
 export const yellow = "#eeca46";
 export const blue = "#4bb5ea";
@@ -6,6 +8,8 @@ export const turquoise = "#4cd6bc";
 export const purple = "#a856ef";
 
 export default {
+  black,
+  grey,
   lightRed,
   red,
   yellow,
--- src/Utilities/transition.js
+++ src/Utilities/transition.js
@@ -1,7 +1,3 @@
-export default ({
-  property = "all",
-  length = "0.5s",
-  ease = "ease-in-out"
-}) => `
+export default ({ property = "all", length = ".2s", ease = "ease-in-out" }) => `
   transition: ${property} ${length} ${ease}
 `;
