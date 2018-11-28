4d8bd5343733710207ea290ca8db8244bc355da2
Denis Barriere <denis.barriere@gmail.com>
2018-11-28 10:51:44 +0100

    Add extra colors

--- src/Utilities/colors.js
+++ src/Utilities/colors.js
@@ -1,19 +1,23 @@
 export const black = "#000";
 export const grey = "#aaa";
+export const lightGrey = "#e5e5e5";
 export const red = "#fd5f65";
 export const lightRed = "#fa715e";
 export const yellow = "#eeca46";
 export const blue = "#4bb5ea";
 export const turquoise = "#4cd6bc";
 export const purple = "#a856ef";
+export const white = "#fff"
 
 export default {
   black,
   grey,
+  lightGrey,
   lightRed,
   red,
   yellow,
   blue,
   turquoise,
-  purple
+  purple,
+  white
 };
